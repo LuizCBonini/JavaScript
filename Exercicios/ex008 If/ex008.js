04c5d62e3b0b64fd4999f95454054f8dbb78194b
Luiz Bonini <lc.bonini13@gmail.com>
2021-11-11 20:28:05 -0300

    Criação de mais 5 exercícios

--- Exercicios/ex008 If/ex008.js
+++ Exercicios/ex008 If/ex008.js
@@ -1,1 +1,5 @@
-console.log('O console funionou corretamente!')
+var vel = 65
+if (vel > 60) {
+    console.log(`Você ultrapassou o limite de velocidade! MULTADO!`)
+}
+console.log(`Dirija sempre com o cinto de segurança!`)
