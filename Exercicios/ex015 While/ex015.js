fc474a34d18b813b953c869ba2b9bf5275d3df44
Luiz Bonini <lc.bonini13@gmail.com>
2021-11-15 00:20:08 -0300

    commit

---
+++ Exercicios/ex015 While/ex015.js
@@ -0,0 +1,16 @@
+var contador = 1
+while (contador < 6) {
+    console.log('Hello world!')
+    contador++
+}
+
+
+
+/*console.log('Hello World!')
+console.log('Hello World!')
+console.log('Hello World!')
+console.log('Hello World!')
+console.log('Hello World!')
+console.log('Hello World!')
+console.log('Hello World!')
+console.log('Hello World!')*/
