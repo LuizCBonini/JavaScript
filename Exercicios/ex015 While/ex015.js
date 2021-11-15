d724b29b7d48bc9a2b6f86119c0e7f384175df88
Luiz Bonini <lc.bonini13@gmail.com>
2021-11-15 00:27:02 -0300

    Update ex015.js

--- Exercicios/ex015 While/ex015.js
+++ Exercicios/ex015 While/ex015.js
@@ -1,10 +1,14 @@
 var contador = 1
+do {
+    console.log('Hello world!')
+    contador++
+} while (contador < 6)
+
+/*var contador = 1
 while (contador < 6) {
     console.log('Hello world!')
     contador++
-}
-
-
+}*/
 
 /*console.log('Hello World!')
 console.log('Hello World!')
