9bbbe6909a34bdb7d84cbf99a65f394665319379
Luiz Bonini <lc.bonini13@gmail.com>
2021-11-17 01:00:27 -0300

    Create funcao01.js

---
+++ Exercicios/ex018 função/funcao01.js
@@ -0,0 +1,17 @@
+function parimpar(n) 
+{
+    if (n % 2 == 0) 
+    {
+        return 'Par!'
+    } else 
+    {
+        return 'Impar!'
+    }
+}
+
+/*var res = parimpar(4)
+console.log(res)*/
+
+// Ou pode usar também:
+
+console.log(parimpar(4))
