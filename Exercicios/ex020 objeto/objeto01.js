48ddc0a476a0851f9620c1f83484f37f1d94e3e7
Luiz Bonini <lc.bonini13@gmail.com>
2021-11-18 21:56:18 -0300

    Create objeto01.js

---
+++ Exercicios/ex020 objeto/objeto01.js
@@ -0,0 +1,9 @@
+var amigo = {nome: 'josé',
+sexo: 'M',
+peso: 85.4,
+engordar(p=0){
+    console.log('Engordou')
+    this.peso += p
+}}
+amigo.engordar(2)
+console.log(`${amigo.nome} pesa ${amigo.peso}KG.`)
