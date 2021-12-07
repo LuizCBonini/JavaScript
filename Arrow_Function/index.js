c6c655730bd1515f88c784040a5e6f3e0147ad9a
Luiz Bonini <lc.bonini13@gmail.com>
2021-12-07 10:42:34 -0300

    teste

--- Arrow_Function/index.js
+++ Arrow_Function/index.js
@@ -1,12 +1,12 @@
 const txtanos = document.getElementById('pidade')
 const anos = Number(txtanos.value)
 
-const maior_idade = (idade) => {
-    if (idade > 18) {
+const maior_idade = (anos) => {
+    if (anos > 18) {
         return "Maior de idade"
     } else {
         return "Menor de idade"
     }
 }
 
-console.log(maior_idade(anos))
+console.log(maior_idade())
